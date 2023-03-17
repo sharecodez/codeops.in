3112835303e8c0ef5c7e85697e82a171ab57b492
rajeshfcgss <113589276+rajeshfcgss@users.noreply.github.com>
2023-03-17 15:55:08 +0530

    Update azure_VM.ps1

--- open_bastion_rdp_using_ps/azure_VM.ps1
+++ open_bastion_rdp_using_ps/azure_VM.ps1
@@ -1,4 +1,4 @@
 ﻿$BastionName = 'bb-nonprd-bastion-host'
 $BastionRG = 'bb-nonprd-rg'
-$VMResourceID= '/subscriptions/aa668178-f7ef-4db1-98b4-e516297ed54e/resourceGroups/bb-nonprd-rg/providers/Microsoft.Compute/virtualMachines/bb-nonprd-vm-01'
+$VMResourceID= '/subscriptions/*****/resourceGroups/bb-nonprd-rg/providers/Microsoft.Compute/virtualMachines/****'
 az network bastion rdp --name $BastionName --resource-group $BastionRG --target-resource-id $VMResourceID
